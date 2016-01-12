22779eef7bb923ccba1534a011dd17d331706f52
yaoyuxiao <531416635@qq.com>
2016-01-12 15:48:10 +0800

    elk的web应用

---
+++ redismqelk-producer/src/main/java/com/zto/entity/mqtest.java
@@ -0,0 +1,53 @@
+package com.zto.entity;
+
+public class mqtest {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private Integer phone;
+
+    private String info;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info == null ? null : info.trim();
+    }
+}
---
+++ redismqelk-producer/src/main/java/com/zto/mapper/mqtestMapper.java
@@ -0,0 +1,30 @@
+package com.zto.mapper;
+
+import com.zto.entity.mqtest;
+import com.zto.entity.mqtestExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface mqtestMapper {
+    int countByExample(mqtestExample example);
+
+    int deleteByExample(mqtestExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(mqtest record);
+
+    int insertSelective(mqtest record);
+
+    List<mqtest> selectByExample(mqtestExample example);
+
+    mqtest selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") mqtest record, @Param("example") mqtestExample example);
+
+    int updateByExample(@Param("record") mqtest record, @Param("example") mqtestExample example);
+
+    int updateByPrimaryKeySelective(mqtest record);
+
+    int updateByPrimaryKey(mqtest record);
+}
